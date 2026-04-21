2481f3847044cbc35e2161c8bc5cfe7c0ea30d7f
vladrob333333-sys <vladrob333333@gmail.com>
2026-04-21 22:30:37 +0300

    main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -5,6 +5,5 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
-    # Получаем все активные услуги с категориями
     services = Service.query.filter_by(is_active=True).order_by(Service.name).all()
     return render_template('index.html', services=services)
