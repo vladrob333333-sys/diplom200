420ba04cda719d4642ce8adaa7f403fb7f13e76d
vladrob333333-sys <vladrob333333@gmail.com>
2026-04-21 20:07:37 +0300

    tickets.js

--- app/static/js/tickets.js
+++ app/static/js/tickets.js
@@ -1,1 +1,9 @@
-// Можно оставить пустым, DataTables инициализируется в шаблонах.
+// Можно оставить пустым или добавить общие функции для DataTables
+$(document).ready(function() {
+    // Автоматическая инициализация DataTables для таблиц с классом .datatable
+    $('.datatable').DataTable({
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/ru.json'
+        }
+    });
+});
